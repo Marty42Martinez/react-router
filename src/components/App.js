504490cce5e70078e7486b5ed1171d4d3f8c7321
Marty Martinez <manuel.marty.martinez@gmail.com>
2019-05-20 11:42:22 -0700

    Added Header component, so we can see things

--- src/components/App.js
+++ src/components/App.js
@@ -6,13 +6,17 @@ import {
 import Red from './colors/Red';
 import Yellow from './colors/Yellow';
 import Blue from './colors/Blue';
+import Header from './Header';
 
 export default function App() {
   return (
     <Router>
-      <Route path="/red" component={Red} />
-      <Route path="/yellow" component={Yellow} />
-      <Route path="/blue" component={Blue} />
+      <>
+        <Header />
+        <Route path="/red" component={Red} />
+        <Route path="/yellow" component={Yellow} />
+        <Route path="/blue" component={Blue} />
+      </>
     </Router>
   );
 }
