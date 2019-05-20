8c7151cee80f9d53ee3f15cc75ca3b6e3e20677b
Marty Martinez <manuel.marty.martinez@gmail.com>
2019-05-20 11:22:06 -0700

    Yellow snapshot passing

---
+++ src/components/colors/Yellow.test.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import Yellow from './Yellow';
+
+describe('Yellow component', () => {
+  it('renders a Yellow', () => {
+    const wrapper = shallow(<Yellow />);
+
+    expect(wrapper).toMatchSnapshot();
+  });
+});
