008886e9d1251f7d43b9d70089e50e2294df96c6
Marty Martinez <manuel.marty.martinez@gmail.com>
2019-05-20 11:22:49 -0700

    Blue component, copied

---
+++ src/components/colors/Blue.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default function Blue() {
+  const style = {
+    backgroundColor: 'blue',
+    width: '100vw',
+    height: '100vh'
+  };
+  
+  return (
+    <>
+      <div style={style}></div>
+    </>
+  );
+}
