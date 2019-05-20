991108a81ff2163f07e25cbbd3a89cca9d1fb560
Marty Martinez <manuel.marty.martinez@gmail.com>
2019-05-20 11:31:03 -0700

    Header, nav with React Links

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+export default function Header() {
+  return (
+    <header>
+      <nav>
+        <Link to='/red'>Red</Link>
+        <Link to='/yellow'>Yellow</Link>
+        <Link to='/blue'>Blue</Link>
+      </nav>
+    </header>
+  );
+}
