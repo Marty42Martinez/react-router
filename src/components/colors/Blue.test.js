b0a6ee2a44ca94a19fdaa7628ef0dacfef8ccb2c
Marty Martinez <manuel.marty.martinez@gmail.com>
2019-05-20 11:23:45 -0700

    Blue snapshot test passing

---
+++ src/components/colors/Blue.test.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import Blue from './Blue';
+
+describe('Blue component', () => {
+  it('renders a Blue', () => {
+    const wrapper = shallow(<Blue />);
+
+    expect(wrapper).toMatchSnapshot();
+  });
+});
